b71b27d8e4631f2e09a7412302641c761f151e1d
Marlon Clay <luciousmc@icloud.com>
2021-06-18 01:16:38 -0700

    HeaderIcon component added styled icons user name displays on right side of header

--- components/Header.js
+++ components/Header.js
@@ -13,10 +13,11 @@ import {
   SearchIcon,
   ShoppingCartIcon
 } from '@heroicons/react/outline';
+import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div>
+    <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
       {/* Left */}
       <div className='flex items-center'>
         <Image
@@ -26,15 +27,34 @@ function Header() {
           layout='fixed'
         />
 
-        <div className='flex'>
+        <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
           <SearchIcon className='h-6 text-gray-600' />
-          <input type="text" placeholder='Search Facebook' />
+          <input
+            type="text"
+            className='hidden md:inline-flex ml-2 items-center bg-transparent outline-none flex-shrink'
+            placeholder='Search The Network'
+          />
         </div>
       </div>
 
       {/* Center */}
+      <div className="flex justify-center flex-grow">
+        <div className='flex space-x-6 md:space-x-2'>
+          <HeaderIcon Icon={HomeIcon} active />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* Right */}
+      <div className='flex items-center sm:space-x-2 justify-end'>
+        {/* Profile Pic */}
+
+        <p className='font-semibold pr-3 whitespace-nowrap'>MarzBarz</p>
+        
+      </div>
     </div>
   )
 }
