766da57cc0844334f950baaed97d1957d5cf9e42
Marlon Clay <luciousmc@icloud.com>
2021-06-18 15:05:03 -0700

    image upload added

--- components/InputBox.js
+++ components/InputBox.js
@@ -3,14 +3,74 @@ import { EmojiHappyIcon } from '@heroicons/react/outline';
 
 import { useSession } from 'next-auth/client';
 import Image from 'next/image';
+import { useRef, useState } from 'react';
+import firebase from 'firebase';
+import { db, storage } from '../firebase';
 
 function InputBox() {
   const [session] = useSession();
+  const inputRef = useRef(null);
+  const filePickerRef = useRef(null);
+  const [imageToPost, setImageToPost] = useState(null);
 
 
   const sendPost = (e) => {
     e.preventDefault();
+
+    if (!inputRef.current.value) return;
+
+    db.collection('posts').add({
+      message: inputRef.current.value,
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    }).then(doc => {
+      if (imageToPost) {
+        const uploadTask = storage
+          .ref(`posts/${doc.id}`)
+          .putString(imageToPost, 'data_url');
+
+        removeImage();
+
+        uploadTask.on(
+          'state_change',
+          null,
+          err => console.error(err),
+          () => {
+            storage
+              .ref(`posts/${doc.id}`)
+              .getDownloadURL()
+              .then(url => {
+                db.collection('posts').doc(doc.id).set({
+                  postImage: url
+                }, {
+                  merge: true
+                });
+              });
+          }
+        );
+      }
+    });
+
+    inputRef.current.value = '';
   }
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0])
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    }
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
+  };
 
   return (
     <div className='bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6'>
@@ -25,12 +85,23 @@ function InputBox() {
 
         <form className="flex flex-1">
           <input
+            ref={inputRef}
             type="text"
             placeholder={`Whats on your mind, ${session.user.name}?`}
             className='rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none'
           />
           <button hidden onClick={sendPost}>Submit</button>
         </form>
+
+        {imageToPost && (
+          <div onClick={removeImage} className='flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer'>
+            <img
+              className='h-10 object-contain'
+              src={imageToPost}
+              alt=""
+            />
+          </div>
+        )}
       </div>
 
       <div className='flex justify-evenly p-3 border-t'>
@@ -40,17 +111,17 @@ function InputBox() {
             Live Video
           </p>
         </div>
-        <div className='inputIcon'>
+        <div
+          onClick={() => filePickerRef.current.click()}
+          className='inputIcon'
+        >
           <CameraIcon className='h-7 text-green-400' />
-          <p className="inputIconText">
-            Photo/Video
-          </p>
+          <p className="inputIconText">Photo/Video</p>
+          <input ref={filePickerRef} type="file" hidden onClick={addImageToPost} />
         </div>
         <div className='inputIcon'>
           <EmojiHappyIcon className='h-7 text-yellow-300' />
-          <p className="inputIconText">
-            Feeling/Activity
-          </p>
+          <p className="inputIconText">Feeling/Activity</p>
         </div>
       </div>
     </div>
